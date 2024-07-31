73d51737c07e927f13242a6272139407d5441e1c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-31 17:36:47 +0700

    handle attach fragment to main activity main

--- app/src/main/java/com/example/demofragmentinandroid/MainActivity.kt
+++ app/src/main/java/com/example/demofragmentinandroid/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.demofragmentinandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.example.demofragmentinandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +16,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
+        if (savedInstanceState == null) {
+            // style java
+//            supportFragmentManager.beginTransaction()
+//                .add(binding.fragmentContainerView.id, FirstFragment().apply {
+//                    arguments = bundleOf("name" to "Minh Khang", "age" to "20")
+//                })
+//                .commit()
 
-        fragmentTransaction.add(binding.fragmentContainerView.id, FirstFragment())
-        fragmentTransaction.commit()
+            // style kotlin
+            supportFragmentManager.commit {
+//                add(binding.fragmentContainerView.id, FirstFragment().apply {
+//                    arguments = bundleOf("name" to "Minh Khang", "age" to "20")
+//                })
+
+                add<FirstFragment>(
+                    containerViewId = binding.fragmentContainerView.id,
+                    args = bundleOf("name" to "Minh Khang", "age" to "20")
+                )
+            }
+        }
     }
 }
