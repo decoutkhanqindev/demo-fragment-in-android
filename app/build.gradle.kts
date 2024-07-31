cf19edb16dd92daf6ac6f08ca07680b6c33949a0
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-31 17:07:03 +0700

    add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.fragment.ktx)
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
 }
