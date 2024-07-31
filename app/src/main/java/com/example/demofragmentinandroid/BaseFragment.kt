28704e33013dcea840eab482feaf8edbda596bc3
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-31 17:27:40 +0700

    create base fragment

--- app/src/main/java/com/example/demofragmentinandroid/BaseFragment.kt
+++ app/src/main/java/com/example/demofragmentinandroid/BaseFragment.kt
@@ -22,6 +22,7 @@ abstract class BaseFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         logMessage("onCreate: savedInstanceState=$savedInstanceState")
+        logMessage("onCreate: arguments=${arguments?.keySet()}")
     }
 
     @CallSuper
